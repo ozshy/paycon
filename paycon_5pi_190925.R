721b70df55ed144e91380fca88ce1d4e17473a70
ozshy <ozshy@ozshy.com>
2020-11-23 12:58:04 -0500

    Initial uploading

---
+++ paycon_5pi_190925.R
@@ -0,0 +1,222 @@
+#paycon_5pi_190915.R Analyzing 5 payment instruments (Fig.1 bottom and Section 4.4 in paper)
+### The following packages are used:
+library(formattable)# has percent function
+#library(plotrix)# weighted histograms
+library(dplyr)
+library(xtable)# for LaTeX tables
+#library(ggplot2)
+#library(car)# for boxCox linearity test
+#library(fitdistrplus)# for Fig 1: Fitting distributions to observed HHI
+#library(actuar)# add Pareto dist (and others) to the above
+setwd("~/Papers/paycon/paycon_coding") # Set working directory!
+dir()
+
+### Reading RDS dataset
+pay1 = readRDS("paycon_16_17_18.rds")# read diary transations
+resp_adoption_3.rds = readRDS("resp_adoption_3.rds")# list of respondents who adopted cash, credit, debit for 3 years
+resp_adoption_5.rds = readRDS("resp_adoption_5.rds")# list of respondents who adopted cash, credit, debit, check, prepaid for 3 years
+objects()
+
+### Restricting to merchants 1:7 and in_person trans only
+table(pay1$in_person)
+pay2 = subset(pay1, in_person==1)
+table(pay2$in_person)
+#
+table(pay2$merch)
+pay2 = subset(pay2, merch %in% 1:7)
+table(pay2$merch)
+
+### Restricting the data to resp who have (adopt) 5 PI: credit, debit, checks, and prepaid cards
+dim(pay2)
+length(unique(pay2$uasid))
+length(resp_adoption_5.rds)
+#
+pay3 = subset(pay2, uasid %in% resp_adoption_5.rds)
+dim(pay3)
+length(unique(pay3$uasid))
+
+### Section 4.4 (5 PI concentration) begins
+nrow(pay3)
+length(unique(pay3$uasid))
+nrow(pay3) - nrow(pay2)# loss of trans by restriction to adoption of all 5 PI
+length(unique(pay3$uasid)) - length(unique(pay2$uasid))# loss of resp by restriction to adoption of all 5 PI
+(length(unique(pay3$uasid)) - length(unique(pay2$uasid)))/length(unique(pay2$uasid))# % loss of resp
+table(pay3$pi)
+nrow(pay3[pay3$pi %in% c(1:5), ])# num trans made with the 5 main PI
+nrow(pay3[pay3$pi %in% c(1:5), ])/nrow(pay3)# frac trans made with the 5 main PI
+nrow(pay3)-nrow(pay3[pay3$pi %in% c(1:5), ])# num trans deleted because not paid with the 5 PI
+# Restricting trans to pi 1:5
+pay4 = subset(pay3, pi %in% c(1:5))# deleting all other PI
+nrow(pay4)
+length(unique(pay4$uasid))
+nrow(pay4) - nrow(pay3)# num payments lost by restricting PI
+length(unique(pay4$uasid)) - length(unique(pay3$uasid)) # num respondents lost by restricting PI
+table(pay4$pi)
+percent(prop.table(table(pay4$pi)))
+sum(table(pay4$pi))# Number of trans 
+
+(fivepi_vol_cash = nrow(pay4[pay4$pi == 1,])  )# num cash trans
+(fivepi_vol_check = nrow(pay4[pay4$pi == 2,])  )# num check trans
+(fivepi_vol_credit = nrow(pay4[pay4$pi == 3,])  )# num credit trans
+(fivepi_vol_debit = nrow(pay4[pay4$pi == 4,])  )# num debit trans
+(fivepi_vol_prepaid = nrow(pay4[pay4$pi == 5,])  )# num prepaid trans
+(fivepi_vol_all = nrow(pay4))
+#
+(fivepi_perc_cash = fivepi_vol_cash/fivepi_vol_all)# vol as % of total num trans
+(fivepi_perc_check = fivepi_vol_check/fivepi_vol_all)# vol as % of total num trans
+(fivepi_perc_credit = fivepi_vol_credit/fivepi_vol_all)# vol as % of total num trans
+(fivepi_perc_debit = fivepi_vol_debit/fivepi_vol_all)# vol as % of total num trans
+(fivepi_perc_prepaid = fivepi_vol_prepaid/fivepi_vol_all)# vol as % of total num trans
+(fivepi_perc_all = fivepi_vol_all/fivepi_vol_all)# vol as % of total num trans
+#
+(fivepi_avg_cash = mean(pay4[pay4$pi == 1,]$amnt, na.rm = T))
+(fivepi_avg_check = mean(pay4[pay4$pi == 2,]$amnt, na.rm = T))
+(fivepi_avg_credit = mean(pay4[pay4$pi == 3,]$amnt, na.rm = T))
+(fivepi_avg_debit = mean(pay4[pay4$pi == 4,]$amnt, na.rm = T))
+(fivepi_avg_prepaid = mean(pay4[pay4$pi == 5,]$amnt, na.rm = T))
+(fivepi_avg_all = mean(pay4$amnt, na.rm = T))
+# Respondents incidence begins
+(fivepi_med_cash = median(pay4[pay4$pi == 1,]$amnt, na.rm = T))
+(fivepi_med_check = median(pay4[pay4$pi == 2,]$amnt, na.rm = T))
+(fivepi_med_credit = median(pay4[pay4$pi == 3,]$amnt, na.rm = T))
+(fivepi_med_debit = median(pay4[pay4$pi == 4,]$amnt, na.rm = T))
+(fivepi_med_prepaid = median(pay4[pay4$pi == 5,]$amnt, na.rm = T))
+(fivepi_med_all = median(pay4$amnt, na.rm = T))
+#
+(fivepi_resp_cash = length(unique(pay4[pay4$pi == 1,]$uasid)))# num resp who pay with cash at least once
+(fivepi_resp_check = length(unique(pay4[pay4$pi == 2,]$uasid)))# num resp who pay with check at least once
+(fivepi_resp_credit = length(unique(pay4[pay4$pi == 3,]$uasid)))# num resp who pay with credit at least once
+(fivepi_resp_debit = length(unique(pay4[pay4$pi == 4,]$uasid)))# num resp who pay with debit at least once
+(fivepi_resp_prepaid = length(unique(pay4[pay4$pi == 5,]$uasid)))# num resp who pay with pp at least once
+(fivepi_resp_all = length(unique(pay4$uasid)))# num resp who uses PI at least once (incidence)
+#
+(fivepi_resp_perc_cash = length(unique(pay4[pay4$pi == 1,]$uasid))/fivepi_resp_all)# % resp who pay with cash at least once
+(fivepi_resp_perc_check = length(unique(pay4[pay4$pi == 2,]$uasid))/fivepi_resp_all)# % resp who pay with check at least once
+(fivepi_resp_perc_credit = length(unique(pay4[pay4$pi == 3,]$uasid))/fivepi_resp_all)# % resp who pay with credit at least once
+(fivepi_resp_perc_debit = length(unique(pay4[pay4$pi == 4,]$uasid))/fivepi_resp_all)# % resp who pay with debit at least once
+(fivepi_resp_perc_prepaid = length(unique(pay4[pay4$pi == 5,]$uasid))/fivepi_resp_all)# % resp who pay with pp at least once
+(fivepi_resp_perc_all = length(unique(pay4$uasid))/fivepi_resp_all)# %S resp who uses PI at least once (incidence)
+# Resp incidence for 5+ trans
+trans_per_resp = pay4 %>% group_by(uasid) %>% summarise("vol" = n())# Num trans made by each resp
+head(trans_per_resp)
+sum(trans_per_resp$vol)# total num trans
+nrow(pay4)# verifying same as above
+trans_per_resp5 = subset(trans_per_resp, vol > 4) # Restricting to resp who made 5 or more trans
+nrow(trans_per_resp5)# num resp with 5+ trans
+# 
+# now, back to constructing Table 1 (Table 1 deleted from paper)
+# below, num resp who made 5+ payments by PI
+nrow(pay4)# Num trans for any respo
+names(trans_per_resp5)
+length(unique(trans_per_resp5$uasid))# num resp who made 5+ payments
+sum(trans_per_resp5$vol)# total num trans made by the above resp
+pay45 = subset(pay4, uasid %in% trans_per_resp5$uasid)## Restricting pay4 to resp with 5+ trans
+nrow(pay45)# total num trans made by resp who made 5+ trans
+head(pay45)
+
+# Renaming payement instruments 
+pay55 = pay45
+pay55 = pay55 %>% mutate(pi = replace(pi, pi==1, "cash"))
+pay55 = pay55 %>% mutate(pi = replace(pi, pi==2, "check"))
+pay55 = pay55 %>% mutate(pi = replace(pi, pi==3, "credit"))
+pay55 = pay55 %>% mutate(pi = replace(pi, pi==4, "debit"))
+pay55 = pay55 %>% mutate(pi = replace(pi, pi==5, "prepaid"))
+str(pay55)# Problem, pi is char not factor, so
+# below make it a factor with the correct levels
+pay55$pi = factor(pay55$pi, levels = c("cash", "check", "credit", "debit", "prepaid"))
+head(pay45$pi, 30)# verifying above mapping of pi is correct
+head(pay55$pi, 30)
+tail(pay45$pi, 30)
+tail(pay55$pi, 30)
+table(pay55$pi)# num transactions by PI
+percent(prop.table(table(pay55$pi)))
+names(pay55)
+
+### Summarizing use (by trans num) of each PI, of each resp
+pay_cash5 = pay55 %>%  group_by(uasid) %>% summarise(sum(pi == "cash"))
+pay_check5 = pay55 %>%  group_by(uasid) %>% summarise(sum(pi == "check"))
+pay_credit5 = pay55 %>%  group_by(uasid) %>% summarise(sum(pi == "credit"))
+pay_debit5 = pay55 %>%  group_by(uasid) %>% summarise(sum(pi == "debit"))
+pay_prepaid5 = pay55 %>%  group_by(uasid) %>% summarise(sum(pi == "prepaid"))
+
+### Total dollar spending (amnt) by each resp
+pay_amnt5 = pay55 %>% group_by(uasid) %>% summarise(sum(amnt))
+head(pay_amnt5)
+
+### Num transactions by each respondent
+pay_trans5 = pay55 %>% group_by(uasid) %>% summarise(n())
+head(pay_trans5)
+summary(pay_trans5)
+table(pay_trans5$`n()`)
+sum(pay_trans5$`n()`)
+nrow(pay55)
+length(unique(pay_trans5$uasid))# num all respondents with both cards
+length(unique(pay55$uasid))# num all respondents with both cards
+
+### merging all the above into a single dataframe
+pay_pi51  = merge(x = pay_trans5, y = pay_cash5, all.x = T)
+pay_pi51b = merge(x = pay_pi51, y = pay_check5, all.x = T)
+pay_pi52  = merge(x = pay_pi51b, y = pay_credit5, all.x = T)
+pay_pi53  = merge(x = pay_pi52, y = pay_debit5, all.x = T)
+pay_pi53a = merge(x = pay_pi53, y = pay_prepaid5, all.x = T)
+pay_pi53b = merge(x = pay_pi53a, y = pay_amnt5, all.x = T)
+dim(pay_pi53b)
+head(pay_pi53b)
+#
+names(pay_pi53b) = c("uasid", "total","cash", "check ", "credit", "debit", "prepaid",  "amount")
+head(pay_pi53b)
+
+### computing the HHI concentraition measure (5 PI)
+# Turning tran num intro fractions
+pay_pi54 = pay_pi53b
+pay_pi54$cash    = pay_pi54$cash/pay_pi54$total
+pay_pi54$check   = pay_pi54$check/pay_pi54$total
+pay_pi54$credit  = pay_pi54$credit/pay_pi54$total
+pay_pi54$debit   = pay_pi54$debit/pay_pi54$total
+pay_pi54$prepaid = pay_pi54$prepaid/pay_pi54$total
+head(pay_pi54)
+# Adding the HHI concentration measure
+pay_pi54$hhi = pay_pi54$cash^2 + pay_pi54$check^2 + pay_pi54$credit^2 + pay_pi54$debit^2 + pay_pi54$prepaid^2
+head(pay_pi54)
+#
+nrow(pay_pi54)# num resp who made at least 5 payments
+sum(pay_pi54$total)# total num trans
+summary(pay_pi54$hhi)# dist of concentration 
+table(pay_pi54$hhi == 1)# num respo with HHI = 1
+percent(prop.table(table(pay_pi54$hhi == 1)))# the above in %
+summary(pay_pi54[pay_pi54$hhi < 1,]$hhi)# dist of concentration for resp with HHI < 1
+
+### HHI histogram (5 pi): Bottom Figure 1 Subsection 4.4
+# 
+# Histogram with density
+histPercent <- function(x, ...) {
+  H <- hist(x, plot = FALSE, breaks = seq(0.2, 1, 0.05))
+  H$density <- with(H, 100 * density* diff(breaks)[1])
+  labs <- paste(round(H$density), "%", sep="")
+  plot(H, freq = FALSE, labels = labs, xlim=c(0.2,1), ylim=c(0, 1.08*max(H$density)),...)# Oz added the xlim here to improve x-axis
+}
+par(mar = c(4.5,4,2,0))
+histPercent(pay_pi54$hhi, col = "lightblue",  ylab = "Percentage of respondents", xlab = "Payment concentration (HHI) by volume with 5 payment methods", main = "")
+
+# Information (stats) for Figure 1 bottom
+# num of resp and trans in the above histograms
+length(unique(pay_pi54$uasid))# num resp in top of Fig 1
+sum(pay_pi54$total)# num trans of the above resp
+summary(pay_pi54$hhi)
+length((pay_pi54[pay_pi54$hhi == 1, ]$uasid))# num resp with HHI=1
+percent(length((pay_pi54[pay_pi54$hhi == 1, ]$uasid)))/length(unique(pay_pi54$uasid))# 
+
+length(unique(pay_pi54$uasid))# num resp
+sum(pay_pi54$total)# num trans
+median(pay_pi54$hhi)
+mean(pay_pi54$hhi)
+
+# Info for HHI = 1 
+names(pay_pi54)
+pay45_hhi1 = pay45[pay45$uasid %in% pay_pi54[pay_pi54$hhi == 1,]$uasid, ]
+nrow(pay45_hhi1)# total num trans for HHI=1
+length(unique(pay45_hhi1$uasid))# num resp with HHI=1
+table(pay45_hhi1$pi)# Amazing! Resp with hhi=1 concentration on 3 PI only!
+100*prop.table(table(pay45_hhi1$pi)) # Amazing! All those who 100% cocentrated did not concentrate on Prepaid or Checks.
+
+### End of coding for 5 PI (Section 4.4 and Fig 1 bottom in paper) ###
---
+++ paycon_use_200113.R
@@ -0,0 +1,247 @@
+# paycon_use_200110.R 2nd revision for REIO: New Table 1 (use of PI regardless of adoption)
+
+### The following packages are used:
+library(formattable)# has percent function
+#library(plotrix)# weighted histograms
+library(dplyr)
+#library(ineq) # for Gini coefficient
+#library(mfx) marginal effects for logit
+#library(broom) # for tidy lm summary
+library(xtable)# for LaTeX tables
+#library(ggplot2)
+#library(car)# for boxCox linearity test
+#library(fitdistrplus)# for Fig 1: Fitting distributions to observed HHI
+#library(actuar)# add Pareto dist (and others) to the above
+setwd("~/Papers/paycon/paycon_coding") # Set your working directory !!!
+dir()
+
+### Reading RDS dataset
+pay1 = readRDS("paycon_16_17_18.rds")# read diary transation based
+#resp_adoption_3 = readRDS("resp_adoption_3.rds")# list of respondents who adopted cash, credit, debit for 3 years
+#resp_adoption_5 = readRDS("resp_adoption_5.rds")# list of respondents who adopted cash, credit, debit, check, prepaid for 3 years
+objects()
+
+## Restricting to in_person trans only
+table(pay1$in_person)
+pay2 = subset(pay1, in_person==1)
+table(pay2$in_person)
+
+### Restricting trans to 7 merchant types only#
+table(pay2$merch)
+pay2 = subset(pay2, merch %in% 1:7)
+table(pay2$merch)
+nrow(pay2)# num trans reported in Sec. 2
+length(unique(pay2$uasid))# num respondents
+
+# Restricting the sample to 5pi 1=cash, 2=check, 3=credit, 4=debit, 5=prepaid
+pay3 = pay2
+table(pay3$pi)
+nrow(pay3[pay3$pi %in% c(1:5), ])# num trans made with the 5 main PI
+nrow(pay3[pay3$pi %in% c(1:5), ])/nrow(pay3)# frac trans made with the 5 main PI
+nrow(pay3)-nrow(pay3[pay3$pi %in% c(1:5), ])# num trans deleted because not paid with the 5 PI
+# Restricting trans to 5 pi
+pay4 = subset(pay3, pi %in% c(1:5))# deleting all other PI
+nrow(pay4)
+length(unique(pay4$uasid))
+nrow(pay4) - nrow(pay3)# num payments lost by restricting PI
+length(unique(pay4$uasid)) - length(unique(pay3$uasid)) # num respondents lost by restricting PI
+table(pay4$pi)
+percent(prop.table(table(pay4$pi)))
+
+# adding demographics ==> Not needed here b/c Table 1 focuses on use only
+demog1 = subset(pay4, select = c("uasid", "work_employed", "work_onleave", "work_temp_unemployed", "work_looking", "work_retired", "work_disabled", "work_other", "work_occupation", "work_self", "marital_status", "highest_education", "hispaniclatino", "hispaniclatino_group", "race_white",   "race_black", "race_asian", "race_other", "hh_size", "age", "income_hh", "gender"))
+names(demog1)
+dim(demog1)
+head(demog1, 3)
+str(demog1)
+#
+# edit demographics
+pay5 = pay4
+table(pay5$work_employed)
+pay5$work = pay5$work_employed # create a new variable "work"
+pay5$work[pay5$work==1] = "employed"
+pay5$work[pay5$work==0] = "not_employed"
+table(pay5$work)
+pay5$work = factor(pay5$work)
+table(pay5$work)
+#
+table(pay5$marital_status)
+str(pay5$marital_status)
+pay5$marital = pay5$marital_status # create a new variable "marital"
+pay5$marital[pay5$marital==1 | pay5$marital==2] = "married"
+pay5$marital[pay5$marital %in% c(3,4,5,6)] = "not_married"
+table(pay5$marital)
+pay5$marital = factor(pay5$marital)
+#
+table(pay5$highest_education)
+pay5$education = NA # new variable "education"
+pay5$education[pay5$highest_education <= 9] = "HS_or_less"
+pay5$education[pay5$highest_education == 10] = "Some_college"
+pay5$education[pay5$highest_education >= 11 & pay5$highest_education <= 12] = "Assoc_degree"
+pay5$education[pay5$highest_education == 13 ] = "BA_degree"
+pay5$education[pay5$highest_education >= 14] = "MA_or_higher"
+#
+table(pay5$education)
+pay5$education = factor(pay5$education, levels = c("HS_or_less", "Some_college", "Assoc_degree", "BA_degree", "MA_or_higher")) 
+levels(pay5$education)
+table(pay5$education)
+#
+table(pay5$hh_size) # no edit
+table(pay5$age) # no edit
+summary(pay5$income_hh) # no edit
+#
+table(pay5$gender)
+pay5$gender[pay5$gender==0] = "female"
+pay5$gender[pay5$gender==1] = "male"
+table(pay5$gender)
+pay5$gender = factor(pay5$gender, levels = c("female", "male"))
+table(pay5$gender)
+#
+str(pay5)
+head(pay5)
+dim(pay5)
+#
+length(unique(pay5$uasid))
+
+
+# Table 1 use statistics (regardless of adoption)
+pay6 = pay5
+(num_payments = nrow(pay6))# num of payments
+(num_resp =length(unique(pay6$uasid)))# num respondents
+(total_val = sum(pay6$amnt))# total dollar value of all payments
+
+table(pay6$pi)
+pay6[pay6$pi==1, "pi"] = "cash"
+pay6[pay6$pi==2, "pi"] = "check"
+pay6[pay6$pi==3, "pi"] = "credit"
+pay6[pay6$pi==4, "pi"] = "debit"
+pay6[pay6$pi==5, "pi"] = "prepaid"
+table(pay6$pi)
+
+# Volume share of payments by PI
+(frac_cash = nrow(pay6[pay6$pi=="cash",])/num_payments)
+(frac_check = nrow(pay6[pay6$pi=="check",])/num_payments)
+(frac_credit = nrow(pay6[pay6$pi=="credit",])/num_payments)
+(frac_debit = nrow(pay6[pay6$pi=="debit",])/num_payments)
+(frac_prepaid = nrow(pay6[pay6$pi=="prepaid",])/num_payments)
+
+# value shoare of payments by PI
+(frac_val_cash = sum(subset(pay6, pi=="cash")$amnt)/total_val)
+(frac_val_check = sum(subset(pay6, pi=="check")$amnt)/total_val)
+(frac_val_credit = sum(subset(pay6, pi=="credit")$amnt)/total_val)
+(frac_val_debit = sum(subset(pay6, pi=="debit")$amnt)/total_val)
+(frac_val_prepaid = sum(subset(pay6, pi=="prepaid")$amnt)/total_val)
+
+# Avg payment value by PI
+(avg_val_cash = mean(subset(pay6, pi=="cash")$amnt))
+(avg_val_check = mean(subset(pay6, pi=="check")$amnt))
+(avg_val_credit = mean(subset(pay6, pi=="credit")$amnt))
+(avg_val_debit = mean(subset(pay6, pi=="debit")$amnt))
+(avg_val_prepaid = mean(subset(pay6, pi=="prepaid")$amnt))
+
+# Median payment value by PI
+(med_val_cash = median(subset(pay6, pi=="cash")$amnt))
+(med_val_check = median(subset(pay6, pi=="check")$amnt))
+(med_val_credit = median(subset(pay6, pi=="credit")$amnt))
+(med_val_debit = median(subset(pay6, pi=="debit")$amnt))
+(med_val_prepaid = median(subset(pay6, pi=="prepaid")$amnt))
+
+# Highest payment value by PI
+(max_val_cash = max(subset(pay6, pi=="cash")$amnt))
+(max_val_check = max(subset(pay6, pi=="check")$amnt))
+(max_val_credit = max(subset(pay6, pi=="credit")$amnt))
+(max_val_debit = max(subset(pay6, pi=="debit")$amnt))
+(max_val_prepaid = max(subset(pay6, pi=="prepaid")$amnt))
+
+# Lowest payment value by PI
+(min_val_cash = min(subset(pay6, pi=="cash")$amnt))
+(min_val_check = min(subset(pay6, pi=="check")$amnt))
+(min_val_credit = min(subset(pay6, pi=="credit")$amnt))
+(min_val_debit = min(subset(pay6, pi=="debit")$amnt))
+(min_val_prepaid = min(subset(pay6, pi=="prepaid")$amnt))
+
+# num payments made at this merchant type
+(vol_merch1 = nrow(subset(pay6, merch==1)))
+(vol_merch2 = nrow(subset(pay6, merch==2)))
+(vol_merch3 = nrow(subset(pay6, merch==3)))
+(vol_merch4 = nrow(subset(pay6, merch==4)))
+(vol_merch5 = nrow(subset(pay6, merch==5)))
+(vol_merch6 = nrow(subset(pay6, merch==6)))
+(vol_merch7 = nrow(subset(pay6, merch==7)))
+
+# fraction merch type 1:7 is paid with cash
+(frac_merch1_cash = nrow(subset(pay6, merch==1 & pi=="cash"))/vol_merch1)
+(frac_merch2_cash = nrow(subset(pay6, merch==2 & pi=="cash"))/vol_merch2)
+(frac_merch3_cash = nrow(subset(pay6, merch==3 & pi=="cash"))/vol_merch3)
+(frac_merch4_cash = nrow(subset(pay6, merch==4 & pi=="cash"))/vol_merch4)
+(frac_merch5_cash = nrow(subset(pay6, merch==5 & pi=="cash"))/vol_merch5)
+(frac_merch6_cash = nrow(subset(pay6, merch==6 & pi=="cash"))/vol_merch6)
+(frac_merch7_cash = nrow(subset(pay6, merch==7 & pi=="cash"))/vol_merch7)
+
+# fraction merch type 1:7 is paid with check
+(frac_merch1_check = nrow(subset(pay6, merch==1 & pi=="check"))/vol_merch1)
+(frac_merch2_check = nrow(subset(pay6, merch==2 & pi=="check"))/vol_merch2)
+(frac_merch3_check = nrow(subset(pay6, merch==3 & pi=="check"))/vol_merch3)
+(frac_merch4_check = nrow(subset(pay6, merch==4 & pi=="check"))/vol_merch4)
+(frac_merch5_check = nrow(subset(pay6, merch==5 & pi=="check"))/vol_merch5)
+(frac_merch6_check = nrow(subset(pay6, merch==6 & pi=="check"))/vol_merch6)
+(frac_merch7_check = nrow(subset(pay6, merch==7 & pi=="check"))/vol_merch7)
+
+# fraction merch type 1:7 is paid with credit
+(frac_merch1_credit = nrow(subset(pay6, merch==1 & pi=="credit"))/vol_merch1)
+(frac_merch2_credit = nrow(subset(pay6, merch==2 & pi=="credit"))/vol_merch2)
+(frac_merch3_credit = nrow(subset(pay6, merch==3 & pi=="credit"))/vol_merch3)
+(frac_merch4_credit = nrow(subset(pay6, merch==4 & pi=="credit"))/vol_merch4)
+(frac_merch5_credit = nrow(subset(pay6, merch==5 & pi=="credit"))/vol_merch5)
+(frac_merch6_credit = nrow(subset(pay6, merch==6 & pi=="credit"))/vol_merch6)
+(frac_merch7_credit = nrow(subset(pay6, merch==7 & pi=="credit"))/vol_merch7)
+
+# fraction merch type 1:7 is paid with debit
+(frac_merch1_debit = nrow(subset(pay6, merch==1 & pi=="debit"))/vol_merch1)
+(frac_merch2_debit = nrow(subset(pay6, merch==2 & pi=="debit"))/vol_merch2)
+(frac_merch3_debit = nrow(subset(pay6, merch==3 & pi=="debit"))/vol_merch3)
+(frac_merch4_debit = nrow(subset(pay6, merch==4 & pi=="debit"))/vol_merch4)
+(frac_merch5_debit = nrow(subset(pay6, merch==5 & pi=="debit"))/vol_merch5)
+(frac_merch6_debit = nrow(subset(pay6, merch==6 & pi=="debit"))/vol_merch6)
+(frac_merch7_debit = nrow(subset(pay6, merch==7 & pi=="debit"))/vol_merch7)
+
+# fraction merch type 1:7 is paid with prepaid
+(frac_merch1_prepaid = nrow(subset(pay6, merch==1 & pi=="prepaid"))/vol_merch1)
+(frac_merch2_prepaid = nrow(subset(pay6, merch==2 & pi=="prepaid"))/vol_merch2)
+(frac_merch3_prepaid = nrow(subset(pay6, merch==3 & pi=="prepaid"))/vol_merch3)
+(frac_merch4_prepaid = nrow(subset(pay6, merch==4 & pi=="prepaid"))/vol_merch4)
+(frac_merch5_prepaid = nrow(subset(pay6, merch==5 & pi=="prepaid"))/vol_merch5)
+(frac_merch6_prepaid = nrow(subset(pay6, merch==6 & pi=="prepaid"))/vol_merch6)
+(frac_merch7_prepaid = nrow(subset(pay6, merch==7 & pi=="prepaid"))/vol_merch7)
+
+# finalizing Table 1
+(use_var.vec = c("Volume share (%)", "Value share (%)", "Average payment amount ($)", "Median payment amount ($)", "Highest payment amount ($)", "Lowest payment amount ($)", "Volume share merchant 1 (%)", "Volume share merchant 2 (%)", "Volume share merchant 3 (%)", "Volume share merchant 4 (%)", "Volume share merchant 5 (%)", "Volume share merchant 6 (%)", "Volume share merchant 7 (%)"))
+#
+(use_cash.vec = c(100*frac_cash, 100*frac_val_cash, avg_val_cash, med_val_cash, max_val_cash, min_val_cash, 100*frac_merch1_cash, 100*frac_merch2_cash, 100*frac_merch3_cash, 100*frac_merch4_cash, 100*frac_merch5_cash, 100*frac_merch6_cash, 100*frac_merch7_cash))
+#
+(use_check.vec = c(100*frac_check, 100*frac_val_check, avg_val_check, med_val_check, max_val_check, min_val_check, 100*frac_merch1_check, 100*frac_merch2_check, 100*frac_merch3_check, 100*frac_merch4_check, 100*frac_merch5_check, 100*frac_merch6_check, 100*frac_merch7_check))
+#
+(use_credit.vec = c(100*frac_credit, 100*frac_val_credit, avg_val_credit, med_val_credit, max_val_credit, min_val_credit, 100*frac_merch1_credit, 100*frac_merch2_credit, 100*frac_merch3_credit, 100*frac_merch4_credit, 100*frac_merch5_credit, 100*frac_merch6_credit, 100*frac_merch7_credit))
+#
+(use_debit.vec = c(100*frac_debit, 100*frac_val_debit, avg_val_debit, med_val_debit, max_val_debit, min_val_debit, 100*frac_merch1_debit, 100*frac_merch2_debit, 100*frac_merch3_debit, 100*frac_merch4_debit, 100*frac_merch5_debit, 100*frac_merch6_debit, 100*frac_merch7_debit))
+#
+(use_prepaid.vec = c(100*frac_prepaid, 100*frac_val_prepaid, avg_val_prepaid, med_val_prepaid, max_val_prepaid, min_val_prepaid, 100*frac_merch1_prepaid, 100*frac_merch2_prepaid, 100*frac_merch3_prepaid, 100*frac_merch4_prepaid, 100*frac_merch5_prepaid, 100*frac_merch6_prepaid, 100*frac_merch7_prepaid))
+
+# verify that shares sum up to 100%
+(use_cash.vec + use_check.vec + use_credit.vec + use_debit.vec + use_prepaid.vec)
+
+#
+(use.df = data.frame(use_var.vec, use_cash.vec, use_check.vec, use_credit.vec, use_debit.vec, use_prepaid.vec))
+dim(use.df)
+# below, create matrix w\ 1 extra column to indicate number of digits for each row
+(digitm = matrix(c(rep(1,6+1), rep(1,6+1), rep(2,6+1) , rep(2,6+1) , rep(2,6+1) , rep(2,6+1), rep(1,6+1), rep(1,6+1), rep(1,6+1), rep(1,6+1), rep(1,6+1), rep(1,6+1), rep(1,6+1)), nrow = 13, ncol = 6+1, byrow = T))
+#
+print(xtable(use.df, digits = digitm), include.rownames = F, hline.after = c(0,2,6,13))
+
+
+
+
+
+#### End of paycon coding file (3 PI, Sections 2, 3, 4.2, 4.3, 4.5, 5.1 in paper) ####
+###################################
+
